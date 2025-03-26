f568e7623b762ed7ed482b7e442d1cf7999ae5ca
shivam2534 <134926520+shivamkanchole@users.noreply.github.com>
2025-03-26 10:26:46 +0530

    added build script in backend

--- workwise_backend/src/lib/prisma.ts
+++ workwise_backend/src/lib/prisma.ts
@@ -1,14 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  // Correctly extend the globalThis interface
   namespace NodeJS {
     interface Global {
       prisma?: PrismaClient;
     }
   }
 
-  // Or extend globalThis (preferred in newer TypeScript versions)
   var prisma: PrismaClient | undefined;
 }
 
