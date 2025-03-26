6043fffde6e31c6d2835c1c5a93fe5837e61a568
shivam2534 <134926520+shivamkanchole@users.noreply.github.com>
2025-03-27 02:32:21 +0530

    changes backend url to test

--- workwise_frontend/src/app/constant.ts
+++ workwise_frontend/src/app/constant.ts
@@ -1,5 +1,6 @@
 // export const HTTP_BACKEND_URL = "http://localhost:3000"
-// export const HTTP_BACKEND_URL = "https://ticket-booking-system-5lks.onrender.com"
-
 export const HTTP_BACKEND_URL =
-  "http://ec2-43-204-142-134.ap-south-1.compute.amazonaws.com:3000";
+  "https://ticket-booking-system-5lks.onrender.com";
+
+// export const HTTP_BACKEND_URL =
+//   "http://ec2-43-204-142-134.ap-south-1.compute.amazonaws.com:3000";
