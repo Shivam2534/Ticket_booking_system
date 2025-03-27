23ed55b76d10076d559ab9c6392e656925484cbf
shivam2534 <134926520+shivamkanchole@users.noreply.github.com>
2025-03-27 12:29:30 +0530

    nginx works fine, update backend link to the aws

--- workwise_frontend/src/app/constant.ts
+++ workwise_frontend/src/app/constant.ts
@@ -1,6 +1,10 @@
+// This is for local development
 // export const HTTP_BACKEND_URL = "http://localhost:3000"
-export const HTTP_BACKEND_URL =
-  "https://ticket-booking-system-5lks.onrender.com";
 
+// backend is deplyed over the render
 // export const HTTP_BACKEND_URL =
-//   "http://ec2-43-204-142-134.ap-south-1.compute.amazonaws.com:3000";
+//   "https://ticket-booking-system-5lks.onrender.com";
+
+// Backend is deployed over the AWS with CD pipeline
+export const HTTP_BACKEND_URL =
+  "http://ec2-43-204-142-134.ap-south-1.compute.amazonaws.com";
