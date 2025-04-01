fee4cb91d80d5294a1da586a8e91edf4b324b6aa
shivam2534 <134926520+shivamkanchole@users.noreply.github.com>
2025-04-01 16:21:31 +0530

    change backend url to onrender deployed backend just to test bug( website is not running on phone, but it work fine over the pc/laptop)

--- workwise_frontend/src/app/constant.ts
+++ workwise_frontend/src/app/constant.ts
@@ -2,9 +2,9 @@
 // export const HTTP_BACKEND_URL = "http://localhost:3000"
 
 // backend is deplyed over the render
-// export const HTTP_BACKEND_URL =
-//   "https://ticket-booking-system-5lks.onrender.com";
+export const HTTP_BACKEND_URL =
+  "https://ticket-booking-system-5lks.onrender.com";
 
 // Backend is deployed over the AWS with CD pipeline (IT SHOWS NOT SHAFE FOR NOW , BECAUSE WE DON'T HAVE CERTIFICATE, WE CAN FIX IT LATER)
-export const HTTP_BACKEND_URL =
-  "https://ec2-43-204-142-134.ap-south-1.compute.amazonaws.com";
+// export const HTTP_BACKEND_URL =
+//   "https://ec2-43-204-142-134.ap-south-1.compute.amazonaws.com";
